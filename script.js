37b382b1cf95b649e80b306ad0df2d0a2f89bda8
charlesmalimba <charlesmalimba@gmail.com>
2026-01-11 23:43:32 +0300

    Add search functionality for music tracks

---
+++ script.js
@@ -0,0 +1,32 @@
+const search = document.getElementById("search");
+const results = document.getElementById("results");
+const audio = document.getElementById("audio");
+const cover = document.getElementById("cover");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+
+search.addEventListener("keyup", () => {
+  if (search.value.length > 2) {
+    fetch(`https://api.jamendo.com/v3.0/tracks/?client_id=709fa152&format=json&limit=30&namesearch=${search.value}`)
+      .then(res => res.json())
+      .then(data => {
+        results.innerHTML = "";
+        data.results.forEach(song => {
+          const div = document.createElement("div");
+          div.innerHTML = `
+            <img src="${song.album_image}" width="150">
+            <h4>${song.name}</h4>
+            <p>${song.artist_name}</p>
+          `;
+          div.onclick = () => {
+            audio.src = song.audio;
+            audio.play();
+            cover.src = song.album_image;
+            title.innerText = song.name;
+            artist.innerText = song.artist_name;
+          };
+          results.appendChild(div);
+        });
+      });
+  }
+});
